b07707129ec5d212bcd02ff3b87d290320d0a6ea
initVD <vaidikpipaliya@gmail.com>
2025-11-27 22:09:15 +0530

    preparing frntend

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import speech_recognition as sr
+import eel
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -10,24 +11,23 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+@eel.expose
 def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
-        audio = r.listen(source,10,6)
+        audio = r.listen(source, 10, 6)
 
     try:
-        print("Recogniing....")
+        print('Recognizing....')
         query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
-
+        print(f"user said: {query}")
+        speak(query)
     except Exception as e:
         return ""
     return query.lower()
-text = takecommand()
 
-speak("Assistant is starting up")
 
 
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import eel
 from engine.features import *
+from engine.command import *
 
 eel.init('www')
 playAssistantSound()
 
-os.system('"start chrome.exe --app="http://localhost:8000/index.html"')
+os.system('"start msedge.exe --app="http://localhost:8000/index.html"')
 eel.start('index.html', mode=None, host='localhost', block=True)
